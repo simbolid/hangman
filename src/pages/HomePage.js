72ffeafb5014debdff06d3ae8b6451ce9c1f36c8
Daniel Simboli <das817@lehigh.edu>
2022-12-12 15:24:47 -0500

    fix class name warning

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,34 +1,36 @@
-
 import { useNavigate } from 'react-router-dom';
 import './HomePage.css'
-function HomePage(){
-    let navigate = useNavigate();
-    function handleSP(){
-        navigate("/sp")
-    }
-    function handleMP(){
-        navigate("/mp")
-    }
 
-    return (
-       <div id = "container">
-        <div class = "TitleText">
-        <font size="+3"> <b> Welcome to Hangman! </b>  </font> <br/> <br/> <font size="+1"> &nbsp; &nbsp; &nbsp; &nbsp;Select your Option </font>
+const HomePage = () => {
+  const  navigate = useNavigate();
+
+  function handleSP() {
+    navigate("/sp")
+  }
+
+  function handleMP() {
+    navigate("/mp")
+  }
+
+  return (
+    <div id="container">
+      <div className="TitleText">
+        <font size="+3"> <b> Welcome to Hangman! </b>  </font> <br /> <br /> <font size="+1"> &nbsp; &nbsp; &nbsp; &nbsp;Select your Option </font>
+      </div>
+      <div className="options">
+        <div id="sp">
+          <button className="button-30" onClick={() => handleSP()}>
+            Single-Player
+          </button>
         </div>
-        <div class = "options"> 
-            <div id = "sp"> 
-            <button class = "button-30" onClick={() => handleSP()}>
-                Single-Player
-            </button>
-            </div>
-            <div id = "mp"> 
-            <button class = "button-30"  onClick={() => handleMP()}>
-                Multi-Player
-            </button>
-            </div>
+        <div id="mp">
+          <button className="button-30" onClick={() => handleMP()}>
+            Multi-Player
+          </button>
         </div>
-        </div>
-    )
+      </div>
+    </div>
+  )
 
 }
 export default HomePage;
