411d54183a33213abe343750beb4637dfd8675ea
IliasChekkori <ilc224@lehigh.edu>
2022-12-12 12:34:29 -0500

    socket initiliazed on port 8080 client-side

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-import { useState, useEffect, Component} from 'react';
-import HangMan from './components/hangMan/HangMan';
-import LetterTable from "./components/letterTable/LetterTable";
-import getWord from './services/randomWord';
-import { w3cwebsocket as W3CWebSocket} from 'websocket'; 
-import Search from 'antd/es/transfer/search';
-import Lobby from './pages/Lobby/Lobby';
-
-const client = new W3CWebSocket("ws://127.0.0.1:8080");
-const App = () => {
-  const [word, setWord] = useState('');
-     // choose a random word when the app first loads
-    useEffect(() => {
-      const rand_word = getWord();
-      setWord(rand_word);
-      client.onopen = () => { 
-        console.log("Connected to the Websocket client connected"); 
-      };
-  
-      client.onmessage = (message) => {
-        const dataFromServer = JSON.parse(message.data);
-        console.log('got reply', dataFromServer);
-      }
-    }, []);  // [] tells React to only run this once
-  
-  return (
-    <>
-      <h1>{word}</h1>
-      <LetterTable />
-      <Lobby client={client}/>
-=======
 import { useState, useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import HangmanGraphic from './components/HangManGraphic';
@@ -39,6 +7,13 @@ import getRandomWord from './services/randomWord';
 import 'react-toastify/dist/ReactToastify.css';
 import { useNavigate } from 'react-router-dom';
 import './App.css'
+import LetterTable from "./components/letterTable/LetterTable";
+import getWord from './services/randomWord';
+import { w3cwebsocket as W3CWebSocket} from 'websocket'; 
+import Search from 'antd/es/transfer/search';
+import Lobby from './pages/Lobby/Lobby';
+
+const client = new W3CWebSocket("ws://127.0.0.1:8080");
 const App = () => {
   let navigate = useNavigate();
   const [word, setWord] = useState(getRandomWord());
@@ -160,7 +135,6 @@ const App = () => {
       </button>
       </div>
       </div>
->>>>>>> f1b02fa630598c01b5fe82f629028ac6137764b9
     </>
   );
 };
