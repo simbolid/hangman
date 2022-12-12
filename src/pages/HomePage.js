7bb4feb9f8fe9e63b3f4e657d09d16769b8039f8
Naod <naa224@lehigh.edu>
2022-12-11 23:44:14 -0500

    homepage

---
+++ src/pages/HomePage.js
@@ -0,0 +1,32 @@
+
+import './HomePage.css'
+function HomePage(){
+    function handleSP(){
+        alert("IN HERE");
+    }
+    function handleMP(){
+        alert("IN HERqE");
+    }
+
+    return (
+       <div id = "container">
+        <div class = "TitleText">
+        <font size="+3"> <b> Welcome to Hangman! </b>  </font> <br/> <br/> <font size="+1"> &nbsp; &nbsp; &nbsp; &nbsp;Select your Option </font>
+        </div>
+        <div class = "options"> 
+            <div id = "sp"> 
+            <button class = "button-30" onClick={() => handleSP()}>
+                Single-Player
+            </button>
+            </div>
+            <div id = "mp"> 
+            <button class = "button-30"  onClick={() => handleMP()}>
+                Multi-Player
+            </button>
+            </div>
+        </div>
+        </div>
+    )
+
+}
+export default HomePage;
