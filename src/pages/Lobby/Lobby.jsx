106a6098ec1ad16944a74417b41b3ba04fbc5203
Naod <naa224@lehigh.edu>
2022-12-12 15:07:56 -0500

    Adding Stuff

--- src/pages/Lobby/Lobby.jsx
+++ src/pages/Lobby/Lobby.jsx
@@ -1,18 +1,21 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import Search from 'antd/es/transfer/search';
-
+import './Lobby.css'
 const Lobby = ({ client }) => {
   const [user, setUser] = useState('');
   const navigate = useNavigate();
-
+  const [code, setCode] = useState('');
   const onButtonClick = (message) => {
     client.send(JSON.stringify({
       type: "message",
       value: message
     }));
   };
-
+  const handleS = (e) =>{
+    //code for what to do with the code
+    alert(code);
+  }
   const createRoom = (id) => {
 
     client.send(JSON.stringify({
@@ -26,13 +29,19 @@ const Lobby = ({ client }) => {
   return (
     <>
       <h1>
-        Enter or create game room
+        Enter or Create A Room
       </h1>
       <div>
         <button onClick={() => createRoom()}>
-          Create Game Room
+          Create Room
         </button>
-        <button onClick={() => onButtonClick("Hello!")}>send button</button>
+      </div>
+      <div>
+        <form onSubmit={() => handleS()}>
+        <input id = "code" placeholder='Ex. 1234' value={code} onChange = {(e) => setCode(e.target.value)}>
+        </input>
+        <button id = "submit" type="submit"><b><font size="+1"> Submit </font> </b></button>
+        </form>
       </div>
     </>
   )
