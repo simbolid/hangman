437fe9ba43c95db75759f12442f751458c42e490
IliasChekkori <ilc224@lehigh.edu>
2022-12-12 16:45:45 -0500

    socket.io final

--- src/pages/Lobby/Lobby.jsx
+++ src/pages/Lobby/Lobby.jsx
@@ -3,86 +3,51 @@ import { useNavigate } from 'react-router-dom';
 import { nanoid } from 'nanoid';
 import { io } from 'socket.io-client';
 import "./Lobby.css"
-let socket
-const ENDPOINT = 'http://localhost:8081'
+import Chat from './Chat';
+
+
+
+const socket = io.connect("http://localhost:3001");
 
 
 const Lobby = () => {
-
-
-  const navigate = useNavigate();
-  const [room, setRoomCode] = useState('');
-
-   socket = io.connect(ENDPOINT)
-
-   /* useEffect(() => { 
-        const connectionOptions = {
-            "forceNew" : true,
-            "reconnectionAttempts": "Infinity", 
-            "timeout" : 10000,                  
-            "transports" : ["websocket"]
-        }
-        socket = io.connect(ENDPOINT, connectionOptions); 
-
-        socket.emit('join', {room: room}, (error) => {
-            if (error)
-                setRoomFull(true)
-        })
-
-        return function cleanup() {
-            socket.emit('disconnect')
-            //shut down connnection instance
-            socket.off()
-        }
-    }, [])*/
-
-    const sendMessage = () => {
-        socket.emit("message", {message: "Hello"})
-    }
-
-
+    const [username, setUsername] = useState("");
+    const [room, setRoom] = useState("");
+    const [showChat, setShowChat] = useState(false);
   
-  const createRoom = () => {
-    /*
-    const gameId = nanoid(6);
-
-    client.send(JSON.stringify({
-        type: "createRoom",
-        value: gameId
-    }));
-
-    navigate('/room', { state: gameId });
-    */
-  };
-
-
-  const enterRoom = () => {
-    //code for what to do with the code
-   // console.log(roomCode);
-  }
-
-  return (
-    <>
-      <h1>
-        Enter or Create A Room
-      </h1>
-      <div>
-        <button onClick={() => sendMessage()}>sendMessage</button>
-        <button onClick={() => createRoom()}>
-          Create Room
-        </button>
-        <form onSubmit={() => enterRoom()}>
-         <input 
-           id ="code" 
-           placeholder='ID of room to join' 
-           value={123} 
-           onChange = {(e) => setRoomCode(e.target.value)}>
-         </input>
-         </form>
+    const joinRoom = () => {
+      if (username !== "" && room !== "") {
+        socket.emit("join_room", room);
+        setShowChat(true);
+      }
+    };
+  
+    return (
+      <div className="App">
+        {!showChat ? (
+          <div className="joinChatContainer">
+            <h3>Join A Chat</h3>
+            <input
+              type="text"
+              placeholder="John..."
+              onChange={(event) => {
+                setUsername(event.target.value);
+              }}
+            />
+            <input
+              type="text"
+              placeholder="Room ID..."
+              onChange={(event) => {
+                setRoom(event.target.value);
+              }}
+            />
+            <button onClick={joinRoom}>Join A Room</button>
+          </div>
+        ) : (
+          <Chat socket={socket} username={username} room={room} />
+        )}
       </div>
-      
-    </>
-  );
+    );
 };
 
 export default Lobby;
--- src/services/server.js
+++ src/services/server.js
@@ -1,153 +1,32 @@
-const { addUser, removeUser, getUser, getUsersInRoom } = require('./users')
-const express = require('express')
-const socketio = require('socket.io')
-const http = require('http')
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
-const PORT = 8081; 
+app.use(cors());
 
-const app = express()
 const server = http.createServer(app);
-const io = socketio(server);
 
-app.use(cors())
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
 
-io.on("connection", socket => {
-    socket.on("join", (payload, callback) =>{
-        let numberOfUsersInRoom = getUsersInRoom(payload.room).length; 
+  socket.on("join_room", (data) => {
+    socket.join(data);
+  });
 
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+});
 
-        const {error, newUser} = addUser({
-            id: socket.id,
-            name: numberOfUsersInRoom===0 ? 'Player 1' : 'Player 2',
-            room: payload.room
-        })
-
-        if(error)
-            return callback(error)
-
-        socket.join(newUser.room);
-
-
-        io.to(newUser.room).emit('roomData', {room: newUsers.room, users: getUsersInRoom(newUser.room)})
-        socket.emit('currentUser', {name: newUser.name})
-
-        callback(); 
-    })
-
-
-    socket.on('initGameState', gameState => { 
-        const user = getUser(socket.id);
-        if(user)
-            io.to(user.name).emit('updateGameState', gameState)
-    })
-
-    socket.on('updateGameState', gameState => {
-        const user = getUser(socket.id)
-        if(user)
-            io.to(user.room).emit('updateGameState', gameState)
-    })
-
-
-    socket.on('disconnect', () => {
-        const user = removeUser(socket.id)
-        if(user)
-            io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)})
-    })
-
-    socket.on('message', (data) => {
-       socket.to(data.room).emit("message", data)
-    })
-
-    //Once we connect to the socket: 
-
-    console.log("user connected", socket.id)
-})
-
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
-
-/*const httpServer = http.createServer(); 
-httpServer.listen(webSocketServerPort, () => console.log("Listening on port" +  webSocketServerPort));
-
-
-const wsServer = new websocketServer({
-    "httpServer": httpServer
-})
-const clients = {}
-const rooms = {}
-wsServer.on("request", request => {
-     //generate a new clientId
-    const clientId = guid(); 
-    console.log((new Date()) + " Recieved a new connection from the origin " + request.origin);
-
-    //connect 
-    const connection = request.accept(null, request.origin);
-    clients[clientId] = connection;
-    console.log("connected: " + clientId + ' in ' + Object.getOwnPropertyNames(clients));
-
-
-    //on message handler: 
-    connection.on('message', function(message){
-        if (message.type == 'utf8'){
-            console.log("Recieved Message: ", message.utf8Data); 
-
-            for (key in clients){
-                clients[key].sendUTF(message.utf8Data); 
-                console.log("sent message to ", clientId); 
-            }
-        }
-    })
-
-
-    //create room handler
-    connection.on('createRoom', function(message){
-        if (message.type == 'utf8'){
-            console.log("room created", message.utf8Data);
-            const roomId = message.utf8Data; 
-
-            rooms.push({id: roomId.id, numUsers: 1, usersInRoom: [roomId.userId]})
-
-
-          
-        }
-
-
-
-        
-    })
-
-    
-    const payload = { 
-        "method": "connect",
-        "clientId": clientId
-    }
-    connection.send(JSON.stringify(payload));
-
-})
-
-const addUser = ({id, name, room}) => {
-    const numberOfUsersInRoom = room.status; 
-
-    if(numberOfUsersInRoom > 2)
-        return { error: 'Room full' }
-
-    const newUser = { id, name, room }
-    clients.push(newUser)
-    return { newUser }
-}
-
-
-
-
-
-function S4() {
-    return (((1+Math.random())*0x10000)|0).toString(16).substring(1); 
-}
- 
-// then to call it, plus stitch in '4' in the third group
-const guid = () => (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
-
-*/
+server.listen(3001, () => {
+  console.log("SERVER IS RUNNING");
+});
